bc578dccb6e4fe165391f8da505e87fdc3e83515
Liam Duggan <dugganl1@tcd.ie>
2024-09-01 22:39:24 +0100

    working again

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { FavoriteFood } from './FavoriteFood'
 
 export const App = () => {
   return (
     <>
       <h1>Hello World</h1>
-      <FavoriteFood />
       <p className="read-the-docs">
-        I love the world and my favorite food!
+        I love the world
       </p>
     </>
-  );
-};
+  )
+}
 
