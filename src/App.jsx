ce65266012b75430b565d3b8e80490342c6f8baf
Liam Duggan <dugganl1@tcd.ie>
2024-09-01 22:44:03 +0100

    added FavoriteFood.jsx to export const

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import { FavoriteFood } from './FavoriteFood.jsx'
 
 export const App = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <FavoriteFood />
       <p className="read-the-docs">
-        I love the world
+        I love the world and my favorite food!
       </p>
     </>
   )
---
+++ src/FavoriteFood.jsx
@@ -0,0 +1,8 @@
+export const FavoriteFood = () => {
+  return (
+    <div>
+      <h2>My Favorite Food</h2>
+      <p>Pizza with extra cheese and mushrooms!</p>
+    </div>
+  );
+};
