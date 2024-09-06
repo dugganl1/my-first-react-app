0b82dcad6cd8b517820544bcfdd7df14d93777ff
Liam Duggan <dugganl1@tcd.ie>
2024-09-06 17:45:43 +0100

    added some work on state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 import { List } from "./List";
 
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+
 export const App = () => {
-  const animals = [
-    "Lion",
-    "Cow",
-    "Snake",
-    "Lizard",
-    "Elephant",
-    "Tiger",
-    "Zebra",
-    "Monkey",
-    "Elephant",
-    "Tiger",
-    "Zebra",
-    "Cheetah",
-    "Wolf",
-    "Fox",
-    "Rabbit",
-    "Deer",
-    "Bear",
-  ];
+  const animals = ["Lion", "Cow", "Snake", "Lizard", "Monkey", "Elephant", "Tiger"];
+
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [numChanges, setNumChanges] = useState(0);
+
+  const onButtonClick = (color) => () => {
+    if (color !== backgroundColor) {
+      setBackgroundColor(color);
+      setNumChanges((prevCount) => prevCount + 1);
+    }
+  };
 
   return (
-    <>
-      <h1>Animals: </h1>
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      <h1>Animals starting with L:</h1>
       <List animals={animals} />
-    </>
+
+      <div className="color-buttons">
+        {COLORS.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={onButtonClick(color)}
+            className={backgroundColor === color ? "selected" : ""}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
+      <p>Number of changes: {numChanges}</p>
+    </div>
   );
 };
